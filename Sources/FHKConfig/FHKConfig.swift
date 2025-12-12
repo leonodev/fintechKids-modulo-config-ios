94a5132e41f2a73b921394c43a8eee50650d1a27
Fredy Leon <fredy.leon@ua.la.com>
2025-12-12 16:32:20 +0100

    add model Configuration

--- Sources/FHKConfig/FHKConfig.swift
+++ Sources/FHKConfig/FHKConfig.swift
@@ -1,2 +1,19 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public struct Configuration {
+    
+    public enum EnvironmentType: String {
+        case production = "production"
+        case development = "development"
+    }
+
+    public enum LanguageType: String {
+        case en = "EN"
+        case es = "ES"
+        case it = "IT"
+        case fr = "FR"
+    }
+}
